2e042ed4c5d212e14efd3bd6a041e9a0f64e0393
Jesus Borobia Sánchez <43232430+jesusborobia3@users.noreply.github.com>
2018-10-04 09:32:35 +0200

    Add files via upload

---
+++ par_impar.py
@@ -0,0 +1,8 @@
+def par_impar(): 
+    numero=(input('Introduce un numero entero')) 
+    if numero%2 == 0 :
+         print 'Este numero es par'
+    else:
+        print"Este numero es impar"
+    
+par_impar()
