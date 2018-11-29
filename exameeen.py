176f328e1184f116de2901da2b263604d8c84aae
Jesus Borobia Sánchez <43232430+jesusborobia3@users.noreply.github.com>
2018-11-29 15:33:53 +0100

    Add files via upload

---
+++ exameeen.py
@@ -0,0 +1,44 @@
+# Este programa sirve para #
+def programa():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    num3 = float(input("Enter third number: "))
+    num4 = float(input("Enter fourth number: "))
+    num5 = float(input("Enter fifth number: "))
+    num6 = float(input("Enter sixth number: "))
+    num7 = float(input("Enter seventh number:"))
+    num8 = float(input("Enter eigth number: "))
+    num9 = float(input("Enter ninth number: "))
+    num10 = float(input("Enter tenth number: "))
+    for i in range(100000): 
+        if(num1<=i):
+           print str(num1)
+           break
+        elif(num2<=i):
+            print str(num2)
+            break
+        elif(num3<=i):
+           print str(num3)
+           break
+        elif(num4<=i):
+           print str(num4)
+           break
+        elif(num5<=i):
+           print str(num5)
+           break
+        elif(num6<=i):
+           print str(num6)
+           break
+        elif(num7<=i):
+           print str(num7)
+           break
+        elif(num8<=i):
+           print str(num8)
+           break
+        elif(num9<=i):
+           print str(num9)
+           break
+        elif(num10<=i):
+           print str(num10)
+           break
+programa()
